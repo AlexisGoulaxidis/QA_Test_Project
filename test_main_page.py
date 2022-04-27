5bd41d3393327b4e2754de94a13c2e11ff809ab9
AlexisGoulaxidis <alexisgoulaxidis@gmail.com>
2022-04-27 14:12:28 -0400

    Main_Page_Testing

--- test_main_page.py
+++ test_main_page.py
@@ -20,18 +20,9 @@ class TestLoginFromMainPage():
 	    page = LoginPage(browser, link)
 	    page.open()
 	    page.should_be_login_link()
-
-
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-
-
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = BasketPage(browser, link)
-    page.open()
     
-    
-@pytest.mark.regression
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 
     link = 'http://selenium1py.pythonanywhere.com/ru/'
     page = BasketPage(browser, link)
