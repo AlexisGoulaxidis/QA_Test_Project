9e015eab5dd018510aed5bd440a07ea4cbe5ebff
AlexisGoulaxidis <97764007+AlexisGoulaxidis@users.noreply.github.com>
2022-04-28 19:57:17 +0300

    Base page Main Class and functions

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,11 +7,12 @@ from .locators import BasePageLocators
 
 
 class BasePage():
-	def __init__(self, browser, url, timeout=7):
+	def __init__(self, browser, url, timeout=2):
 	
 		self.browser = browser
 		self.url = url
 		self.browser.implicitly_wait(timeout)
+		self.wait = WebDriverWait(browser, 4)
 	
 	def open(self):
 	
@@ -55,9 +56,7 @@ class BasePage():
 	def is_disappeared(self, how, what, timeout=4):
 		try:
 			WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how, what)))
-			
 		except TimeoutException:
-		
 			return False
 		
 		return True
