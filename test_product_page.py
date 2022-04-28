81a1d44319881e3dc26db68555796983e809c7fb
AlexisGoulaxidis <97764007+AlexisGoulaxidis@users.noreply.github.com>
2022-04-28 19:58:47 +0300

    Product Page Testing Code

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
 from .pages.product_page import BasketPage
 from .pages.login_page import LoginPage
+from .pages.cart_page import CartPage
 from random import randrange
 import time
 
@@ -11,19 +12,19 @@ import time
                                    # "?promo=offer0",
                                    # "?promo=offer1",
                                    # "?promo=offer2",
-                                   # "?promo=offer3",
+                                   # "?promo=offer3",              #cart_page.py
                                    # "?promo=offer4",
                                    # "?promo=offer5",
                                    # "?promo=offer6",
                                    pytest.param("?promo=offer7",
                                                 marks=pytest.mark.xfail(reason="We are working on that!")),
                                    # "?promo=offer8",
-                                   "?promo=offer9"])
+                                   #"?promo=offer9"
+                                   ])
 def test_guest_can_add_product_to_basket(browser,promo):
 	
-	browser.delete_all_cookies()
-	
 	try:
+		browser.delete_all_cookies()
 		link = f'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/{promo}'
 		page = BasketPage(browser, link)
 		page.open()
@@ -37,7 +38,6 @@ def test_guest_can_add_product_to_basket(browser,promo):
 	except:
 		raise AssertionError
 		print(f'Problem at this link: {link}')
-
 
 
 @pytest.mark.smoke
@@ -97,14 +97,15 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 
 	browser.delete_all_cookies()
+	
 	link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
-	page = BasketPage(browser, link)
+	page = CartPage(browser, link)
 	page.open()
 	page.go_to_basket_page()
 	page.should_not_be_items_in_basket()
 	page.should_be_text_about_empty_basket()
 
-@pytest.mark.need_review
+
 class TestUserAddToBasketFromProductPage():
 
 
@@ -120,8 +121,6 @@ class TestUserAddToBasketFromProductPage():
 		page.register_new_user(rand_email, rand_password)
 		page.should_be_authorized_user()
 		
-		yield
-		browser.delete_all_cookies()
 	
 	@pytest.mark.need_review
 	def test_user_can_add_product_to_basket(self, browser):
@@ -140,6 +139,8 @@ class TestUserAddToBasketFromProductPage():
 			print(f'Problem at this link: {link}')
 			raise AssertionError
 			
+
+
 
 	def test_user_cant_see_success_message(self, browser):
 		
